91bd3694a56c2e0db53e46a46b158b8b7fd21ba0
Dmitriy <dima.kub.yg@gmail.com>
2022-10-18 12:01:20 +0300

    feat(ring_buffer): add ring buffer module

---
+++ Core/Src/drv/ring_buffer/ring_buffer.c
@@ -0,0 +1,55 @@
+/**
+  ******************************************************************************
+  * @file    ring_buffer.c
+  * @brief   Ring Buffer Module.
+  ******************************************************************************
+  *
+  * Author: dmitriy
+  * Created on: Oct 18, 2022
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "ring_buffer.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+
+/* Private define ------------------------------------------------------------*/
+//#define  DUTY_CYCLE_SHIFT         10U    /*!< duty cycle shift */
+
+/* Private macro -------------------------------------------------------------*/
+
+
+/* Private variables ---------------------------------------------------------*/
+
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+/* Private user code ---------------------------------------------------------*/
+
+
+/* External variables --------------------------------------------------------*/
+
+
+/* External functions --------------------------------------------------------*/
+
+/**
+  * @brief TIM3 Initialization Function of PWM Led
+  * This function configures timer that change led state
+  * @param Led: PwmLed object
+  * @param htim: timer handle
+  * @param Channel: channel of timer
+  * @param Pulse: initial PWM pulse
+  * @retval None
+  */
+
+
+
+
+
+
+
+
---
+++ Core/Src/drv/ring_buffer/ring_buffer.h
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file           : ring_buffer.h
+  * @brief          : Header for ring_buffer.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  *
+  * Author: dmitriy
+  * Created on: Oct 18, 2022
+  *
+  ******************************************************************************
+  */
+
+#ifndef SRC_DRV_RING_BUFFER_RING_BUFFER_H_
+#define SRC_DRV_RING_BUFFER_RING_BUFFER_H_
+
+
+/* Includes ------------------------------------------------------------------*/
+//#include "main.h"
+
+
+/* Exported types ------------------------------------------------------------*/
+
+
+/* Exported constants --------------------------------------------------------*/
+
+
+/* Exported macro ------------------------------------------------------------*/
+
+
+/* Exported functions prototypes ---------------------------------------------*/
+
+
+/* Private defines -----------------------------------------------------------*/
+
+
+
+
+#endif /* SRC_DRV_RING_BUFFER_RING_BUFFER_H_ */
